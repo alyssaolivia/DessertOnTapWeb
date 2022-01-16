1a2941e08e63dc5fc4c9b2b9af398f6aca74548c
Elysian Atlys <31394611+alyssaolivia@users.noreply.github.com>
2022-01-16 18:56:36 -0400

    Update in EntityRelationship of Customer ID

--- Models/Order.cs
+++ Models/Order.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Web;
@@ -8,8 +9,11 @@ namespace DessertOnTapWeb.Models
 {
     public class Order
     {
+        [Key]
         [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int ID { get; set; }
+        [ForeignKey("Customer.ID")]
+        public int ClientID { get; set; }
         public virtual ICollection<OrderItem> OrderItems { get; set; }
         public decimal SubTotal { get; set; }
         public decimal Tax { get; set; }
