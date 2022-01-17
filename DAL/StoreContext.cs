95760d0d59ec6e0ce19e2593fea80b32b767bb70
Elysian Atlys <31394611+alyssaolivia@users.noreply.github.com>
2022-01-16 20:33:16 -0400

    Added StoreContext

---
+++ DAL/StoreContext.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using DessertOnTapWeb.Models;
+
+namespace DessertOnTapWeb.DAL
+{
+    public class StoreContext : DbContext
+    {
+
+        public StoreContext() : base("StoreContext")
+        {
+        }
+
+        public DbSet<Customer> Customers { get; set; }
+        public DbSet<Order> Orders { get; set; }
+        public DbSet<OrderItem> OrderItems { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+        }
+    }
+}
